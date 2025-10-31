01fc989f39339447fa8e4831a3b612fe30536f07
Jeff Allen <jeffallen89@gmail.com>
2025-10-31 10:16:44 -0400

    res mob data updates and texT

--- src/routes/briefs/activity-participation/+page.svelte
+++ src/routes/briefs/activity-participation/+page.svelte
@@ -12,7 +12,9 @@
 	import GraphicsMultiples from '$lib/GraphicMultiples.svelte';
 	import Footer from '$lib/Footer.svelte';
 	import ScrollyImages from "$lib/ScrollyImages.svelte";
-	
+
+
+	import imageBus from "./assets/ttc-bus-1080.jpg";
 	
 	import Footnote from '$lib/Footnote.svelte';
 	import Footnotes from '$lib/Footnotes.svelte';
@@ -115,34 +117,36 @@
 	<div class="text">
 
 		<AuthorDate
-			authors="<a href='' target='_blank'>Author Name</a> & <a href='' target='_blank'>Author Name</a>"
+			authors="<a href='' target='_blank'>Author Name</a> <a href='' target='_blank'>Author Name</a> & <a href='' target='_blank'>Author Name</a>"
 			date="November 2025"
 		/>
 
 	</div>
 
 	<ImageSingle
-		imageURL="https://jamaps.github.io/photos/picimgs/taipei1_2025.jpg"
-		caption="Taipei skyline."
-		source="Jeff Allen."
+		imageURL={imageBus}
+		caption=""
+		source='Photo by <a href="https://unsplash.com/@json_pix?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Jason Ng</a> on <a href="https://unsplash.com/photos/a-red-and-white-bus-parked-on-the-side-of-a-street-bm5UpO1_y2w?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>.'
 		altText=""
 		maxWidth="680px"
+		link="No"
 	/>
 
 	<div class="text">
 		<p>
-			A core function of urban transportation is to help people take part in everyday life—like getting to work, school, shopping, or visiting friends. Work in transportation planning, and particularly on assessing impacts of new public transit, has mostly focused on metrics like reducing travel times, congestion, and pollution, as well as financial impacts, among others. While these are important, they often overlook a key question: does the system actually help everyone get where they want to go. If there are limited transportation options, it can lead to social exclusion, inability participate in society.
+			A core function of urban transportation is to help people take part in everyday life—such as getting to work, school, shopping, or socializing. Yet work in transportation planning, and particularly on assessing impacts of new public transit and transit-oriented development, has mostly focused on metrics like reduction in travel times, congestion, and emissions, as well as financial impacts, among others. While these are important, they often overlook or don't directly answer a key question: do these developments actually help everyone get to where they want to go? If there are limited transportation options, it can cause risks of social exclusion, which can broadly be defined as inability participate in society (link to a couple Steve papers here).
 		</p>
 	</div>
- 
+
 	<!-- IMAGE SCHEMATIC Transport Social Exclusion -->
 
 	<div class="text">
 		<p>
-			A few years ago, we wrote a paper that explores how increased public transit accessibility improves people’s ability to participate in everyday life in the Greater Toronto and Hamilton Area (GTHA). A core finding of this work is that low-income and car-free households in areas with low transit accessibility were the most affected, lack of quality public transit options were limiting their ability to travel to places and participate in society. On this page, we update some of this analysis with more recent data and specifically look at the social inclusion benefits that living near major rail transit lines can have.
+			We wrote a paper a few years ago (https://doi.org/10.1016/j.trd.2019.102212) that explored how increased transit accessibility to destinations improves people’s ability to participate in everyday life in the Greater Toronto and Hamilton Area (GTHA). A core finding of this work is that low-income and car-free households in areas with low transit accessibility were the most affected, lack of quality public transit options were limiting their ability to travel to places and participate in society. 
 		</p>
 		<p>
-			Activity participation refers to how often people can take part in everyday activities—like going to work or school, shopping, attending appointments, or socializing. This can be quantified from travel survey data by simply counting of how many places, or 'activities' that someone visits per day. 
+			On this page, we update some of this analysis with more recent data and specifically look at the social inclusion benefits that specifically living near major rail transit stations can have.
+			This can be measured via <b>activity participation</b>, a metric which refers to how often people can take part in everyday activities, and can be directly quantified from travel survey data by simply counting of how many activities that someone visits per day. 
 		</p>
 	</div>
 
@@ -150,13 +154,16 @@
 
 	<div class="text">
 		<p>
-			The Transportation Tomorrow Survey (TTS) is the largest travel survey and covers the Greater Golden Horseshoe. The survey includes a 'diary' of where people travel and what the purpose of their trip is (work, school, shopping, etc.). For each person in this survey, we counted their total number of activities. Of course there will be  variations, but by aggregating and looking at averages across different socio-economic groups or between neighbourhoords, it can reveal stark differences. For example, those in households with higher incomes, on average, participate in more activities. Particularly residents who are low income and do not have a motor vehicle. Cars are increasingly expensive, ... , ____. age, immigration, etc. 
+			For our analysis, we used the Transportation Tomorrow Survey (TTS) to estimate activity participation for residents Greater Toronto Area. (The survey is a 5% sample of all households). On average, we find that people travel to and partake in 1.15 activities per day. 
+		</p>
+		<p>
+			Of course there will be idiosyncratic variations at an individual level, but by aggregating and looking at averages across different socio-economic groups or between neighbourhoords, it can reveal stark differences. For example, those in households with higher incomes, on average, participate in more activities. This is particularly residents who are low income and also do not have regular access to a car. People who are in households that do not own or lease a car, and are low-income (less than $60k per year), have an average daily activity participation rate 0.89, 22% less than the overall population.
 		</p>
 	</div>
 
 	<div class="text">
 		<p>
-			We can also map activity participation by neighbourhood. Low areas, which can be called <i>participation desserts</i>, are those with are facing ____ social exclusion?. The map below shows in red areas with low average activity participation, less than 1 activity per day. Inner and outer suburban areas appear to be the lowest: Scarborough, northern Etobicoke, swaths of Markham, Brampton, and Mississauga. Many of these neighbourhoods are also lower-income and _____. 
+			We can also map activity participation by neighbourhood. The map below shows in red areas with very low average activity participation, less than 1 activity per day per person. Certain inner- and outer-suburban areas appear to be the lowest: Scarborough, northern Etobicoke, swaths of Markham, Brampton, and Mississauga. Many of these neighbourhoods are also lower-income and home to recent immigrants.
 		</p>
 	</div>
 
@@ -164,63 +171,66 @@
 
 	<div class="text">
 		<p>
-			But what role does living near a major transit station play? One notable pattern on the map is that central parts of Toronto have very few low participation neighbourhoods, even though many of these are also are home to many low-income residents. Let's take a look at this a bit further, by estimating average participation rates near (with 800m) and not near GO and TTC Subway stations.
+			But what role does living near a major transit station play? One notable pattern on the map is that central parts of Toronto have very few low participation neighbourhoods, even though a number of these are also home to many low-income residents who don't own cars. Let's take a deeper look at this, by estimating average participation rates near (within 800m) and not near GO and TTC Subway stations.
 		</p>
 	</div>
 
 	<!-- CHART - YES NO NEAR TRANSIT FOR ALL AND FOR LOW INC/NO CAR GROUP -->
 
-	<div class="text">
+	<!-- 
+	=== Activities_mean weighted by persons_5up ===
+	Population inside buffer: 940027
+	Weighted mean inside buffer: 1.2023 ± 0.0012
+	Population outside buffer: 5344517
+	Weighted mean outside buffer: 1.1388 ± 0.0005
 
+	=== Activities_mean_nocar_lowinc weighted by person_ind_low_income_no_car_hhld ===
+	Population inside buffer: 84863
+	Weighted mean inside buffer: 1.0098 ± 0.0052
+	Population outside buffer: 175865
+	Weighted mean outside buffer: 0.8362 ± 0.0036 
+	-->
+
+	<div class="text">
 		<p>
-			Describe results
+			Clearly living near a public transit station has benefits for enabling residents to travel to and participate in daily activities, especially for low-income residents who do not have a car. But what about the characteristics of those stations? Some stations have better connections to other parts of the city. Some stations have better mixed-use development that enables local trips (e.g. shopping) without having to rely on a car.
 		</p>
-		
-		<p>
-			Qualities of a TOC, connectivity to the city, to employment, to school and education. But also to the development around the transit station - are there grocery stores you can walk to - is there a daycare, is there a library. In more suburban areas, if you live near a transit station that you can access Downtown reasonably well to go to work, but not other amenities, it may be harder to do things locally.
-		</p>
-		
 	</div>
 
 	<!-- CHART - SHOW DIFFERENCE BY LOWINC NO CAR BY WHAT? GO vs TTC? BY TRANSIT LINE? BY MUNICIPALITY? -->
 
-	<div class="text">
-
-		<p>
-			What our paper showed, limitations. Possible research directions individual models benefits of types of TOD. This meant as mostly descriptive. Types of activities.
-		</p>
-
-	</div>
-
-
-	<div class="text">
-
-		<p>
-			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas consequat lacus eu dolor dapibus sodales. Aenean venenatis metus id eleifend tincidunt. Nulla ut lacus et urna finibus bibendum sit amet et ante. Aliquam tristique, ex sed porttitor hendrerit, ex odio accumsan ex, eu maximus leo quam quis nulla. 
-		</p>
-
-		<p>
-			Cras tincidunt nisi non tempus suscipit. Nullam metus erat, ultrices vitae mauris commodo, placerat sollicitudin sem. In vitae dignissim eros. Phasellus porttitor orci.<Footnote id={addFootnote(fns[0])}/> nisl, vitae iaculis nulla pretium et. Fusce nec tortor erat. Vestibulum pretium nisl et ligula ultrices fringilla.
-		</p>
-
-		<p>
-			Vivamus non finibus erat. Ut quis mi at felis aliquam rhoncus eu eget augue. Nunc convallis, dui et congue suscipit, nisl sapien malesuada ligula, vitae luctus justo ligula finibus diam. Quisque aliquam et lacus vitae venenatis. Duis id vulputate augue, vel posuere ex. Nam fermentum consequat dolor, ac finibus justo finibus sit amet. Nam suscipit egestas tellus, malesuada dignissim neque dignissim sed.<Footnote id={addFootnote(fns[1])} />
-		</p>
-
-		<p>
-			Nunc vel massa turpis. Vivamus id odio ut nulla dignissim molestie. 
-		</p>
-
-	</div>
-
-	
+	               <!-- line_name    population  weighted_mean    moe_95
+0                               ALL   1518.779835       1.092521  0.014390
+1                            Barrie    260.510700       0.534216  0.013754
+2                         Kitchener   3932.227900       0.885277  0.020741
+3                    Lakeshore East   3995.123561       0.872319  0.043705
+4                    Lakeshore West   3061.677946       1.039200  0.010163
+5   Line 1: Yonge-University Subway  46758.174575       1.081490  0.005776
+6     Line 2: Bloor-Danforth Subway  37514.019062       1.052038  0.007930
+7           Line 4: Sheppard Subway   4283.328102       0.801947  0.019787
+8                            Milton   2100.842509       0.959785  0.028004
+9                     Niagara Falls           NaN            NaN       NaN
+10                    Richmond Hill   1113.182734       0.426946  0.039683
+11                      Stouffville   2815.885519       0.690343  0.038593 -->
 
 	<div class="text">
-
-		<Footnotes footnotes={footnotes} />
-		
+		<p>
+			Stations on the TTC Subway lines have higher participation rates than GO Rail stations. This is likely because TTC Subway routes provide connections to many neighbourhoods are much less commuter-focused than GO Rail. As well, many TTC Subway stations are located in walkable neighbourhoods with greater sets of amenities. Conversely, most GO Rail stations were built on historic rail corridors, which have a backdrop of more industrial land-uses, and many are currently surrounded by surface parking lots promoting park-and-ride trips, rather than mixed-use neighbourhoods that would encourage trips by active travel modes.
+		</p>
 	</div>
 
-	<Footer />
+	<!-- PHOTOS? OR MAPS? of TOP AND BOTTOM STATIONS (WHERE N IS HIGH ENOUGH) -->
+
+	<div class="text">
+		<p>
+			Next steps?
+			- correlate station level activity participation with amenity density and/or access to destinations
+			- multivariate analysis (ind. models) to parse out specific impacts of transit oriented development and communities that enable activity participation.
+			- analysis in other cities where data is available
+			- analyze participation in different types of activities
+		</p>
+	</div>
+
+	<!-- <Footer /> -->
 
 </main>
